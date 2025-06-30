7d4a4edc214e6909e2b31fe6355becc2ec39c80a
GiordanoFerreira <giordanopalmezano@gmail.com>
2025-06-30 00:10:16 -0300

    Primeiro commit

---
+++ src/main/java/br/com/postechfiap/fornecedor_service/FornecedorServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.fornecedor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornecedorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postechfiap/fornecedor_service/FornecedorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.fornecedor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
